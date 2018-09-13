1f5737c87a115dd0613ae054ee6b5013a0794f81
scott.gibson <scott.gibson@SL-WL-102320>
2018-09-13 16:17:07 -0700

    Add project files.

---
+++ BirthdayTrackerMVC_V2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BirthdayTrackerMVC_V2.Startup))]
+namespace BirthdayTrackerMVC_V2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
